2e51226be2dc547ce3fae0ee9b8a23c35ce32491
TheHarshShow <45802874+TheHarshShow@users.noreply.github.com>
2019-08-28 01:56:09 +0530

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <math.h>
- 
+#include<unordered_map>
+
 #define io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define PB push_back
 #define MP make_pair
@@ -27,11 +28,7 @@ ll min(ll a, ll b);
 ll max(ll a, ll b);
 ll search(ll arr[], ll n, ll k);
 string findSum(string str1, string str2);
- 
- 
- 
- 
- 
+
  
 string findSum(string str1, string str2) 
 { 
@@ -130,13 +127,7 @@ ll powerrr(ll a, ll b){
  
     ll ans = 1;
  
-    for(ll i = 0; i < b; i++){
- 
- 
-        ans*= a;
- 
- 
-    }
+    for(ll i = 0; i < b; i++)ans*=a;
  
     return ans;
  
@@ -151,8 +142,6 @@ void swapp(ll *a, ll*b){
  
  
 }
- 
- 
  
 ll gcd(ll a, ll b)
 {
@@ -176,418 +165,298 @@ ll max(ll a, ll b){
     if(a > b)return a;
     return b;
 }
+ 
+struct node {
 
-set<char> signs;
-set<char> alphas;
-map<string, ll> words;
-stack<char> brac;
-
-ll flagl;
-
-struct node{
-
-    char a;
-    ll b;
+    string s;
     struct node *left;
     struct node *right;
 
-
 };
+
+
+vector<string> post(string s){
+
+    stack<char> a;
+    vector<string> ans;
+
+    REP(i,0,s.length()){
+
+        if(s[i] != '-' && s[i] != '+' && s[i] != '/' && s[i] != '*' && s[i] != '^' && s[i] != ')' && s[i] != '('){
+
+            string temp;
+            ll j;
+            for(j=i; j<s.length(); j++){
+
+                if(s[j] != '-' && s[j] != '+' && s[j] != '/' && s[j] != '*' && s[j] != '^' && s[j] != ')' && s[j] != '('){
+                    temp+=s[j];
+
+                } else break;
+
+            }
+
+            i=j-1;
+
+            ans.PB(temp);
+
+        } else {
+
+            if(s[i] == '-' || s[i]=='+'){
+
+                if(s[i]=='-' && (i==0 || s[i-1] == '+' || s[i-1] == '-' || s[i-1] == '*' || s[i-1] == '/' || s[i-1] == '^' || s[i-1] == '(')){a.push('~');}
+                else {
+                    if(a.empty())a.push(s[i]);
+                    else{
+
+                        while(!a.empty() && a.top() != '('){
+
+
+                            char x=a.top();
+                            a.pop();
+                            string tempy;
+                            tempy+=x;
+                            ans.push_back(tempy);
+                        }
+                        a.push(s[i]);
+
+                    }
+
+                }
+
+            } else if(s[i] == '*' || s[i] == '/'){
+
+                if(a.empty())a.push(s[i]);
+                else{
+
+                    if(a.top() == '+' || a.top() == '-' || a.top()=='(')a.push(s[i]);
+                    else{
+
+                        while(!a.empty() && a.top() != '(' && a.top() != '+' && a.top() != '-'){
+
+                            char x=a.top();
+                            a.pop();
+                            string tempy;
+                            tempy+=x;
+                            ans.push_back(tempy);
+
+                        }
+                        a.push(s[i]);
+
+
+                    }
+
+                }
+
+            } else if(s[i] == '(')a.push(s[i]);
+            else if(s[i] == ')'){
+
+                while(a.top() != '('){
+
+                    char x=a.top();
+                    a.pop();
+                    string tempy;
+                    tempy+=x;
+                    ans.PB(tempy);
+                }
+                a.pop();
+
+
+            } else if(s[i] == '^'){
+
+                if(a.empty())a.push(s[i]);
+                else {
+
+                    while(!a.empty() && a.top() == '~'){
+
+                        char x=a.top();
+                        string tempy;
+                        tempy+=x;
+                        a.pop();
+                        ans.PB(tempy);
+
+                    }
+
+                    a.push('^');
+
+                }
+
+            }
+
+            //if(!a.empty())cout<<a.top()<<endl;
+        }
+
+
+    }
+
+    while(!a.empty()){
+
+        string tempy;
+        tempy+=a.top();
+        a.pop();
+        ans.PB(tempy);
+
+    }
+
+    return ans;
+
+
+}
+
+unordered_map<string, ll> values;
+ll flagl;
+
+ll figure(string s){
+
+    if(s[0] == '0' || s[0] == '1' || s[0] == '2' || s[0] == '3' || s[0] == '4' || s[0] == '5' || s[0] == '6' || s[0] == '7' || s[0] == '8' || s[0] == '9')return stoll(s);
+    
+
+    if(values.find(s) == values.end() || values[s]==INF){flagl=1;return INF;}
+    else return values[s];
+
+    
+
+}
 
 struct node *createNode(){
 
     struct node *temp=(struct node *)malloc(sizeof(struct node));
-    temp->a='n';
-    temp->b=-1928;
-
     return temp;
+
+}
+
+void build(struct node **start, vector<string> s){
+
+    stack<struct node *> a;
+
+    REP(i,0,s.size()){
+
+        if(s[i][0] != '+' && s[i][0] != '*' && s[i][0] != '-' && s[i][0] != '/' && s[i][0] != '^' && s[i][0] != '~'){
+
+            struct node *temp=createNode();
+
+            temp->left=NULL;
+            temp->right=NULL;
+            temp->s = s[i];
+            a.push(temp);
+
+        } else{
+
+            if(s[i][0]=='~'){
+
+
+                if(((a.top())->s)[0] != '~')(a.top())->s = "~"+(a.top())->s;
+                else (a.top())->s = ((a.top())->s).substr(1);
+
+            }
+            else{
+
+
+                struct node *temp=createNode();
+                struct node *temp2 = a.top();
+                a.pop();
+                struct node *temp3 = a.top();
+                a.pop();
+                temp->right=temp2;
+                temp->left=temp3;
+                temp->s=s[i];
+                a.push(temp);
+
+            }
+
+
+        }
+
+    }
+
+    *start = a.top();
+
+
+
+}
+
+void inorder(struct node *start){
+
+    if(start == NULL)return;
+    inorder(start->left);
+    cout<<start->s<<endl;
+    inorder(start->right);
 
 }
 
 ll evaluate(struct node *start){
 
-    if(start->a != 'n'){
+    ll x=0;
 
-        if(start->a == '^'){
+    if(start->s=="+")return evaluate(start->left)+evaluate(start->right);
+    else if (start->s=="-") return evaluate(start->left)-evaluate(start->right);
+    else if (start->s=="*") return evaluate(start->left)*evaluate(start->right);
+    else if (start->s=="/") return evaluate(start->left)/evaluate(start->right);
+    else if (start->s=="^") return powerrr(evaluate(start->left), evaluate(start->right));
+    else {
 
-            return powerrr(evaluate(start->left), evaluate(start->right));
-
-        } else if(start->a == '*'){
-
-            return evaluate(start->left)*evaluate(start->right);
-
-        } else if(start->a == '/'){
-
-
-            return evaluate(start->left)/evaluate(start->right);
-
-
-        } else if(start->a=='+'){
-
-            return evaluate(start->left)+evaluate(start->right);
-
-        } else {
-
-            return evaluate(start->left)-evaluate(start->right);
-
-        }
-
-    } else {
-
-        return start->b;
+        if((start->s)[0] =='~')return -1*figure((start->s).substr(1));
+        else return figure((start->s));
 
     }
 
 
 }
 
-void build(string s, struct node *start){
-
-    REP(i,0,s.length()){
-
-         //cout<<'i'<<i<<endl;
-
-        ll columbus=0;
-
-        if(alphas.find(s[i]) != alphas.end()){
-
-            string temp;
-
-            ll j=i;
-            while(j < s.length() && alphas.find(s[j]) != alphas.end()){
-
-                temp+=s[j];
-                j++;
-            }
-
-            //cout<<temp<<endl;
-
-            //cout<<'a'<<" "<<temp<<" "<<temp.length()<<" "<<words[temp]<<endl;
-
-
-            if(words.find(temp) == words.end() || words[temp] == INF){
-
-                //cout<<"haha"<<endl;
-
-                flagl=1;
-                return;
-
-            } else{
-
-                string temp2="("+to_string(words[temp])+")";
-
-                columbus=temp2.length();
-                if(j<s.length())s=s.substr(0, i) +  temp2 + s.substr(j);
-                else s=s.substr(0, i) + temp2;
-
-                //cout<<'c'<<" "<<s<<"ahshkdnak"<<endl;
-
-
-            }
-
-
-            i=i+columbus-1;
-            //cout<<s<<endl;  
-
-        }
-
-    }
-
-
-    /*REP(i,0,s.length()){
-
-        if(signs.find(s[i]) == signs.end()){
-
-            flagl=1;
-            return;
-
-        }
-
-    }*/
-
-    if(s[0]=='-'){
-
-        if(s[1]!='('){start->b=stoll(s);return;}
-        else {if(s[2]=='-')s="("+s.substr(3);else swap(s[0], s[1]);}
-
-    }
-
-    for(ll i=s.length()-1; i>=0; i--){
-
-        //cout<<'a'<<endl;
-
-
-
-        if(s[i] == ')')brac.push(s[i]);
-        if(s[i] == '(')brac.pop();
-
-        if(!(brac.empty()))continue;
-
-        if(s[i]=='+' || s[i]=='-'){
-
-            //cout<<i<<"tatd"<<endl;
-
-            start->a=s[i];
-            string b=s.substr(0,i);
-            string c=s.substr(i+1, s.length()-1-i);
-
-            //cout<<"a"<<b<<" "<<c<<endl;
-
-            start->left=createNode();
-            start->right=createNode();
-            build(b, start->left);
-            build(c, start->right);
-
-            return;
-
-        }
-
-
-    }
-
-    for(ll i=s.length()-1; i>=0; i--){
-
-        //cout<<'b'<<endl;
-
-
-
-        if(s[i] == ')')brac.push(s[i]);
-        if(s[i] == '(')brac.pop();
-
-        if(!(brac.empty()))continue;
-
-        if(s[i]=='*' || s[i]=='/'){
-
-            
-
-
-            start->a=s[i];
-            string b=s.substr(0,i);
-            string c=s.substr(i+1, s.length()-1-i);
-
-            //cout<<"b"<<b<<" "<<c<<endl;
-
-
-            start->left=createNode();
-            start->right=createNode();
-            build(b, start->left);
-            build(c, start->right);
-
-            return;
-
-        }
-
-
-    }
-
-    REP(i,0,s.length()){
-
-        //cout<<'c'<<endl;
-
-
-        if(s[i] == '(')brac.push(s[i]);
-        if(s[i] == ')')brac.pop();
-
-        if(!(brac.empty()))continue;
-
-        if(s[i]=='^'){
-
-            start->a='^';
-            string b=s.substr(0,i);
-            string c=s.substr(i+1, s.length()-1-i);
-
-            //cout<<"c"<<b<<" "<<c<<endl;
-
-
-            start->left=createNode();
-            start->right=createNode();
-            build(b, start->left);
-            build(c, start->right);
-
-            return;
-
-        }
-
-    }
-
-    REP(i,0,s.length()){
-
-        //cout<<'d'<<endl;
-
-
-        if(s[i]=='('){
-
-            stack<char> flag;
-            flag.push('(');
-
-            REP(j, i+1, s.length()){
-
-                if(s[j]=='(')flag.push('(');
-
-                if(s[j]==')'){
-
-                    flag.pop();
-                    
-
-                }
-
-                if(flag.empty()){
-                    string b=s.substr(i+1, j-1-i);
-
-                    //cout<<'d'<<" "<<b<<endl;
-
-                    build(b, start);
-                    return;
-                }
-
-            }
-
-
-
-        }
-
-    }
-
-    start->b=stoll(s);
-
-
-
-}
 
 int main(){
- 
+
+
     io
- 
-    signs.insert('^');
-    signs.insert('+');
-    signs.insert('*');
-    signs.insert('-');
-    signs.insert('/');
-    signs.insert('(');
-    signs.insert(')');
-    signs.insert('0');
-    signs.insert('1');
-    signs.insert('2');
-    signs.insert('3');
-    signs.insert('4');
-    signs.insert('5');
-    signs.insert('6');
-    signs.insert('7');
-    signs.insert('8');
-    signs.insert('9');
-    alphas.insert('a');
-    alphas.insert('b');
-    alphas.insert('c');
-    alphas.insert('d');
-    alphas.insert('e');
-    alphas.insert('f');
-    alphas.insert('g');
-    alphas.insert('h');
-    alphas.insert('i');
-    alphas.insert('j');
-    alphas.insert('k');
-    alphas.insert('l');
-    alphas.insert('m');
-    alphas.insert('n');
-    alphas.insert('o');
-    alphas.insert('p');
-    alphas.insert('q');
-    alphas.insert('r');
-    alphas.insert('s');
-    alphas.insert('t');
-    alphas.insert('u');
-    alphas.insert('v');
-    alphas.insert('w');
-    alphas.insert('x');
-    alphas.insert('y');
-    alphas.insert('z');
-    alphas.insert('A');
-    alphas.insert('B');
-    alphas.insert('C');
-    alphas.insert('D');
-    alphas.insert('E');
-    alphas.insert('F');
-    alphas.insert('G');
-    alphas.insert('H');
-    alphas.insert('I');
-    alphas.insert('J');
-    alphas.insert('K');
-    alphas.insert('L');
-    alphas.insert('M');
-    alphas.insert('N');
-    alphas.insert('O');
-    alphas.insert('P');
-    alphas.insert('Q');
-    alphas.insert('R');
-    alphas.insert('S');
-    alphas.insert('T');
-    alphas.insert('U');
-    alphas.insert('V');
-    alphas.insert('W');
-    alphas.insert('X');
-    alphas.insert('Y');
-    alphas.insert('Z');
-
-
 
     ll n;
     cin>>n;
-
     while(n--){
+        ll t;
+        cin>>t;
 
-        ll q;
-        cin>>q;
-
-        while(q--){
+        while(t--){
 
             flagl=0;
 
-
             string s;
             cin>>s;
+
             ll flag2=0;
-
-
-
             REP(i,0,s.length()){
 
-                if(s[i] == '='){
+                if(s[i]=='='){
                     flag2=1;
+
+                    vector<string> posy=post(s.substr(i+1));
+
                     struct node *start=createNode();
-                    build(s.substr(i+1), start);
 
-                    if(flagl==1){words[s.substr(0,i)]=INF; break;};
-
-                    words[s.substr(0,i)] = evaluate(start);
+                    build(&start, posy);
+                    ll y = evaluate(start);
+                    if(flagl==1)values[s.substr(0,i)] = INF;
+                    else values[s.substr(0,i)]=y;
                     break;
-
                 }
-
             }
-
-            
-
-            /*for(auto itr=words.begin(); itr != words.end(); itr++){
-
-                cout<<'b'<<" "<<itr->first<<" "<<itr->second<<endl;
-
-            }*/
 
             if(flag2)continue;
 
-            struct node *start=createNode();
+            vector<string> pos=post(s);
+            //REP(i,0,pos.size())cout<<pos[i]<<" ";
+            //cout<<enl;
+            struct node *start = createNode();
+            build(&start, pos);
+            //inorder(start);
+            ll y = evaluate(start);
+            if(flagl)cout<<"CANT BE EVALUATED"<<endl;
+            else cout<<y<<endl;
 
-            build(s, start);
-            if(flagl){cout<<"CANT BE EVALUATED"<<endl;continue;}
-
-            ll x=evaluate(start);
-            
-            cout<<x<<endl;
         }
 
-        words.clear();
+        values.clear();
 
     }
 
-  
+
+
 }
